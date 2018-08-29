cbf7424c4e52c9d4e1e5748b388f35c3c2e23588
Jeff Doolittle <jeffdoolittle@gmail.com>
2018-08-29 18:10:26 +0000

    added two stack queue kata

---
+++ QueueKata/QueueKata.Tests/Tests.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Linq;
+using FluentAssertions;
+using Xunit;
+
+namespace QueueKata.Tests
+{
+    public class SimpleStackTests
+    {
+        [Fact]
+        public void first_test()
+        {
+            var stack = new SimpleStack<Item>();
+
+            stack.Count.Should().Be(0);
+
+            Enumerable
+                .Range(0, 10)
+                .Select(x => new Item { Index = x })
+                .ToList()
+                .ForEach(item => stack.Push(item));
+
+            stack.Count.Should().Be(10);
+
+            for (var i = 9; i > -1; i--)
+            {
+                var current = stack.Pop();
+                current.Index.Should().Be(i);
+            }
+
+            Action fail = () => stack.Pop();
+
+            fail.Should().Throw<Exception>().Where(_ => _.Message.Contains("empty"));
+        }
+
+        public class Item
+        {
+            public int Index { get; set; }
+        }
+    }
+
+    public class TwoStackQueueTests
+    {
+        [Fact]
+        public void first_test()
+        {
+            var queue = new TwoStackQueue<Item>();
+
+            queue.Count.Should().Be(0);
+
+            var items = Enumerable
+                .Range(0, 10)
+                .Select(x => new Item { Index = x })
+                .ToList();
+
+            foreach (var item in items)
+            {
+                queue.Enqueue(item);
+            }
+
+            queue.Count.Should().Be(10);
+
+            var peek = queue.Peek();
+
+            peek.Index.Should().Be(0);
+
+            for (var i = 0; i < 10; i++)
+            {
+                var current = queue.Dequeue();
+                current.Index.Should().Be(i);
+            }
+
+            Action fail = () => queue.Dequeue();
+
+            fail.Should().Throw<Exception>().Where(_ => _.Message.Contains("empty"));
+        }
+
+        [Fact]
+        public void second_test()
+        {
+            var queue = new TwoStackQueue<Item>();
+
+            queue.Count.Should().Be(0);
+
+            var first = Enumerable
+                .Range(0, 5)
+                .Select(x => new Item { Index = x })
+                .ToList();
+
+            foreach (var item in first)
+            {
+                queue.Enqueue(item);
+            }
+
+            queue.Count.Should().Be(5);
+
+            var firstPeek = queue.Peek();
+            firstPeek.Index.Should().Be(0);
+
+            var second = Enumerable
+                .Range(5, 5)
+                .Select(x => new Item { Index = x })
+                .ToList();
+
+            foreach (var item in second)
+            {
+                queue.Enqueue(item);
+            }
+
+            queue.Count.Should().Be(10);
+
+            var peek = queue.Peek();
+
+            peek.Index.Should().Be(0);
+
+            for (var i = 0; i < 10; i++)
+            {
+                var current = queue.Dequeue();
+                current.Index.Should().Be(i);
+            }
+
+            Action fail = () => queue.Dequeue();
+
+            fail.Should().Throw<Exception>().Where(_ => _.Message.Contains("empty"));
+        }
+
+        public class Item
+        {
+            public int Index { get; set; }
+        }
+    }
+}
---
+++ QueueKata/QueueKata/Class1.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace QueueKata
+{
+    public interface IQueue<T> : IReadOnlyCollection<T>
+    {
+        void Enqueue(T item);
+        T Peek();
+        T Dequeue();
+    }
+
+    public interface IStack<T> : IReadOnlyCollection<T>
+    {
+        void Push(T item);
+        T Pop();
+    }
+
+    public class SimpleStack<T> : IStack<T>
+    {
+        private readonly List<T> _inner = new List<T>();
+
+        public void Push(T item)
+        {
+            lock (_inner)
+            {
+                _inner.Insert(0, item);
+            }
+        }
+
+        public T Pop()
+        {
+            lock (_inner)
+            {
+                if (_inner.Count == 0)
+                {
+                    throw new Exception("Stack is empty. Nothing to pop.");
+                }
+
+                var top = _inner[0];
+                _inner.RemoveAt(0);
+                return top;
+            }
+        }
+
+        public int Count
+        {
+            get
+            {
+                lock (_inner)
+                {
+                    return _inner.Count;
+                }
+            }
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            lock (_inner)
+            {
+                return _inner.GetEnumerator();
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }        
+    }
+
+    public class TwoStackQueue<T> : IQueue<T>
+    {
+        private readonly object _lock = new object();
+        private readonly SimpleStack<T> _in = new SimpleStack<T>();
+        private readonly SimpleStack<T> _out = new SimpleStack<T>();
+
+        public void Enqueue(T item)
+        {
+            lock (_lock)
+            {
+                _in.Push(item);
+            }
+        }
+
+        public T Peek()
+        {
+            lock (_lock)
+            {
+                Deliver();
+
+                return _out.First();
+            }
+        }
+
+        public T Dequeue()
+        {
+            lock (_lock)
+            {
+                Deliver();
+
+                return _out.Pop();
+            }
+        }
+
+        private void Deliver()
+        {
+            if (_out.Count == 0 && _in.Count > 0)
+            {
+                while (_in.Count > 0)
+                {
+                    _out.Push(_in.Pop());
+                }
+            }
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            lock (_lock)
+            {
+                foreach (var item in _out)
+                {
+                    yield return item;
+                }
+                foreach (var item in _in.Reverse())
+                {
+                    yield return item;
+                }
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        public int Count { get
+            {
+                lock (_lock)
+                {
+                    return _in.Count + _out.Count;
+                }
+            }
+        }
+    }
+}
